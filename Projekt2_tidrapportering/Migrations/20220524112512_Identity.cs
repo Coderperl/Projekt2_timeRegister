21d8b5a8693a354e507632353fb5f22c6ec2c5ed
Per görtz <pelle_gortz@hotmail.com>
2022-05-24 16:20:36 +0200

    Created project controller, and finalized views

---
+++ Projekt2_tidrapportering/Migrations/20220524112512_Identity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekt2_tidrapportering.Migrations
+{
+    public partial class Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
